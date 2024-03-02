6300f6c716e867e62c671a69aff960cc6b3c7977
Edmondbrn <edmond.berne6@gmail.com>
2024-03-02 21:11:27 +0100

    diagramme

---
+++ Td3/Td1.sql
@@ -0,0 +1,6 @@
+-- CREATE TABLE repas( `date` DATE NOT NULL , `client` CHAR(32) NOT NULL );
+-- CREATE TABLE menu( `date` DATE NOT NULL , `plat` CHAR(128) NOT NULL );
+-- CREATE TABLE preference( `personne` CHAR(32) NOT NULL , `plat` CHAR(128) NOT NULL );
+
+
+
---
+++ mysql.sql
@@ -0,0 +1,7 @@
+-- CREATE TABLE emp3(empid INTEGER NOT NULL PRIMARY KEY,
+-- empname TEXT NOT NULL, email NOT NULL);
+
+-- INSERT INTO emp3(empid, empname, email)
+-- VALUES (1, "Sam", "test1@test.com");
+
+SELECT * FROM emp3;
